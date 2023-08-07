91910c51e5b56b744b048106f56bc48008dbdded
subin <subin.sigosoft@gmail.com>
2023-08-07 22:02:02 +0530

    changes

---
+++ lib/Screens/Splash/Splash.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class Splash extends StatelessWidget {
+  const Splash({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+       body: Container(
+        
+       ),
+    );
+  }
+}
